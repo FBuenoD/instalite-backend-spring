8d820e034518b2d3ee1eede5787d45d1b1107f16
FBUENOD <fernandobuenodansi@gmail.com>
2020-08-19 22:28:20 -0300

    Update _DBService.java

--- src/main/java/edu/ifes/ci/si/dw/intalite/services/_DBService.java
+++ src/main/java/edu/ifes/ci/si/dw/intalite/services/_DBService.java
@@ -50,6 +50,8 @@ public class _DBService {
 				StreamUtils.copyToByteArray(getClass().getResourceAsStream("/static/images/postagem/img04.jpg")));
 		Postagem postagem5 = new Postagem(null, "Que mundo Claro!", usuario1, 10, "2020-08-15",
 				StreamUtils.copyToByteArray(getClass().getResourceAsStream("/static/images/postagem/img05.jpg")));
+		Postagem postagem6 = new Postagem(null, "Que mundo Legal!", usuario4, 6, "2020-08-16",
+				StreamUtils.copyToByteArray(getClass().getResourceAsStream("/static/images/postagem/img03.jpg")));
 
 		Comentario comentario1 = new Comentario(null, "Que Foto Maneira", postagem1, usuario1, "2020-08-10");
 		Comentario comentario2 = new Comentario(null, "Que Foto Legal", postagem1, usuario2, "2020-08-11");
@@ -61,11 +63,13 @@ public class _DBService {
 		Comentario comentario8 = new Comentario(null, "Que Foto Escura", postagem4, usuario4, "2020-08-15");
 		Comentario comentario9 = new Comentario(null, "Que Foto Top", postagem5, usuario1, "2020-08-15");
 		Comentario comentario10 = new Comentario(null, "Que Foto Zica", postagem5, usuario2, "2020-08-16");
+		Comentario comentario11 = new Comentario(null, "Que Foto Louca", postagem6, usuario3, "2020-08-16");
+		Comentario comentario12 = new Comentario(null, "Que Foto Legal", postagem6, usuario4, "2020-08-17");
 
 		usuarioRepository.saveAll(Arrays.asList(usuario1, usuario2, usuario3, usuario4));
-		postagemRepository.saveAll(Arrays.asList(postagem1, postagem2, postagem3, postagem4, postagem5));
+		postagemRepository.saveAll(Arrays.asList(postagem1, postagem2, postagem3, postagem4, postagem5, postagem6));
 		comentarioRepository.saveAll(Arrays.asList(comentario1, comentario2, comentario3, comentario4, comentario5,
-				comentario6, comentario7, comentario8, comentario9, comentario10));
+				comentario6, comentario7, comentario8, comentario9, comentario10, comentario11, comentario12));
 
 	}
 }
